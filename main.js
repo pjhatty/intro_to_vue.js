4d738355705725b790b813a2e44c6861a7ea4cbe
pjhatty <pjhattster@gmail.com>
2018-04-07 11:35:21 -0500

    Event Handling

--- main.js
+++ main.js
@@ -11,12 +11,27 @@ var app = new Vue({
     variants: [
       {
         variantId: 2234,
-        variantColor: "green"
+        variantColor: "green",
+        variantImage: "./assets/vmSocks-green-onWhite.jpg"
       },
       {
         variantId: 2235,
-        variantColor: "blue"
+        variantColor: "blue",
+        variantImage: "./assets/vmSocks-blue-onWhite.jpg"
       }
-    ]
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    removeFromCart() {
+      this.cart -= 1
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
+      this.link = variantImage
+    }
   }
 });
