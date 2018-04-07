dd3e3497a4f1c6cc75f59ea92c44ee9049c152f3
pjhatty <pjhattster@gmail.com>
2018-04-07 09:25:01 -0500

    Attribute Binding Challenge

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: "are what all the cool kids wear!"
+    description: "Comfy Coding!",
+    image: './assets/vmSocks-green-onWhite.jpg',
+    link: './assets/vmSocks-green-onWhite.jpg'
   }
+
 });
